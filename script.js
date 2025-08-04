31d267a8d7c193ba7546eca4ab33c941abff2bb4
Divyansh Verma <divyanshmotog5g@gmail.com>
2025-08-04 23:33:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+const allStrongElement = document.querySelectorAll("p strong")
+
 function highlight() {
     //Write your code here
-
+	allStrongElement.forEach((element) => {
+		element.style.color = "rgb(0, 128, 0)"
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	allStrongElement.forEach((element) => {
+		element.style.color = "rgb(0, 0, 0)"
+	})
     
 }
